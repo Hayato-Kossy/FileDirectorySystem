4b3b2caea8ee200f8398ea41bb450fbbc5db1d4d
Hayato Koshi <kshyt2501@icloud.com>
2022-09-15 00:56:58 +0900

    filesystem.js print setContentコマンドの作成

--- fileSystem.js
+++ fileSystem.js
@@ -157,7 +157,7 @@ class FileSystem{
     }
 
     rm(fileName){
-        if (this.currentDir.list.search(fileName) === null　&& this.currentDir.list.head.attribute == "Directory") return "no such file under current directory.";
+        if (this.currentDir.list.search(fileName) === null　|| this.currentDir.list.head.attribute == "Directory") return "no such file under current directory.";
         else if (this.currentDir.list.head.name === fileName){
             this.currentDir.list.popFront();
             return `deleted ${fileName} file.`
@@ -178,39 +178,137 @@ let File = new FileSystem();
 // console.log(List.popFront())
 // console.log(List);
 
-console.log(File.pwd());
-console.log(File.mkdir("R"));
+// console.log(File.pwd());
+// console.log(File.mkdir("R"));
+// console.log(File.mkdir("python"));
+// console.log(File.mkdir("java"));
+// console.log(File.mkdir("javascript"));
+// console.log(File.mkdir("javascript"));
+// console.log(File.mkdir("Go"));
+// console.log(File.pwd());
+// console.log(File.ls());
+// console.log(File.cd("python"));
+// console.log(File.touch("test.R")); 
+// console.log(File.touch("ML.R"));
+// console.log(File.mkdir("Go"));
+// console.log(File.ls());
+// console.log(File.cd(".."));
+// console.log(File.cd(".."));
+// console.log(File.pwd());
+// console.log(File.mkdir("test"));
+// console.log(File.cd("test"));
+// console.log(File.touch("test.py"));
+// console.log(File.mkdir("test2"));
+// console.log(File.touch("test2.py"));
+// console.log(File.pwd());
+// console.log(File.ls());
+// console.log(File.setContent("test.py","こんにちは"));
+// console.log(File.print("test.py"));
+// console.log(File.rm("test.py"));
+// console.log(File.ls());
+// console.log(File.print("test.py"));
+// console.log(File.mkdir("test3"));
+// console.log(File.cd(".."));
+// console.log(File.pwd());
+// console.log(File.ls());
+
+
+
+// 例題
+// pythonディレクトリの下にtest.pyとtest2.pyを作ってlsコマンドで表示してください
+
+console.log("-------不正解--------")
+//間違いの例
+//ディレクトリとファイルの作成
 console.log(File.mkdir("python"));
-console.log(File.mkdir("java"));
-console.log(File.mkdir("javascript"));
-console.log(File.mkdir("javascript"));
-console.log(File.mkdir("Go"));
-console.log(File.pwd());
-console.log(File.ls());
-console.log(File.cd("python"));
-// console.log(File.mkdir("MachineLearning"));
-// console.log(File.cd("MachineLearning"));
-console.log(File.touch("test.R")); 
-console.log(File.touch("ML.R"));
-console.log(File.mkdir("Go"));
-console.log(File.ls());
-console.log(File.cd(".."));
-console.log(File.cd(".."));
-console.log(File.pwd());
-console.log(File.mkdir("test"));
-console.log(File.cd("test"));
 console.log(File.touch("test.py"));
-console.log(File.mkdir("test2"));
-//console.log(File.cd("test2"));
 console.log(File.touch("test2.py"));
-//console.log(File.cd(".."));
-//console.log(File.cd("test2"));
-console.log(File.pwd());
+
+//回答の確認
 console.log(File.ls());
-console.log(File.setContent("test.py","こんにちは"));
-console.log(File.print("test.py"));
-console.log(File.rm("test.py"));
+
+
+console.log("-------正解--------")
+File = new FileSystem();
+//正解の例
+//ディレクトリとファイルの作成
+console.log(File.mkdir("python"));
+console.log(File.cd("python"));
+console.log(File.touch("test.py"));
+console.log(File.touch("test2.py"));
+
+//回答の確認
 console.log(File.ls());
-console.log(File.print("test.py"));
-//console.log(File.mkdir("test3"));
-//console.log(File.pwd());
+
+let CLITextInput = document.getElementById("CLITextInput");
+let CLITextOutputDiv = document.getElementById("CLIOutputDiv");
+
+CLITextInput.addEventListener("keyup", (event) => submitSerch(event));
+
+function submitSerch(event){
+    if (event.key == "Enter"){
+        let parsedStringInputArray = MTools.commandLineParser(CLITextInput.value);
+
+        MTools.appendEchoParagraph(CLITextOutputDiv);
+        CLITextInput.value = '';
+
+        MTools.appendResultParagraph(CLITextOutputDiv, MTools.evaluatedResultsStringFromParsedStringInputArray(parsedStringInputArray));
+    }
+}
+
+class MTools{
+    static commandLineParser(CLITextInputString){
+        let parsedStringInputArray = CLITextInputString.trim().split(" ");
+
+        return parsedStringInputArray;
+    }
+
+    static appendEchoParagraph(parentDiv){
+        parentDiv.innerHTML +=
+            `
+            <p class="m-0"> 
+            <span style='color:green'>student</span>
+            <span style='color:magenta'>@</span>
+            <span style='color:blue'>recursionist</span>
+            : ${CLITextInput.value} 
+            </p>
+            `;
+        return;
+    }
+
+    static appendResultParagraph(parentDiv, message){
+        parentDiv.innerHTML +=
+            `
+            <p class="m-0">
+                <span style='color: turquoise'>MTools</span>: ${message}
+            </p>
+            `;    
+        return;    
+    }
+
+    static evaluatedResultsStringFromParsedStringInputArray(parsedStringInputArray){
+        let result = 0;
+        let argsArray = parsedStringInputArray[2].split(",").map(stringArgument=>Number(stringArgument));
+        let argA = argsArray[0];
+        let argB = argsArray[1];
+        let commandName = parsedStringInputArray[1];
+
+        if (commandName == "add" ) result = argA+argB;
+        else if (commandName == "subtract" ) result = argA-argB;
+        else if (commandName == "multiply" ) result = argA*argB;
+        else if (commandName == "divide" ) result =  argA/argB;
+        else if (commandName == "exp" ) result = Math.pow(argA, argB);
+        else if (commandName == "log" ) result = Math.log(argB)/Math.log(argA);
+        else if (commandName == "sqrt" ) result = Math.sqrt(argA);
+        else if (commandName == "abs" ) result = Math.abs(argA); 
+        else if (commandName == "round" ) result = Math.round(argA);
+        else if (commandName == "ceil" ) result = Math.ceil(argA);
+        else if (commandName == "floor" ) result = Math.floor(argA); 
+        else if (commandName == "fibo" ) result = Option.fibo(argA); 
+        else if (commandName == "gcd" ) result = Option.gcd(argA, argB);
+
+        else console.log("MTools.evaluatedResultsStringFromParsedStringInputArray:: invalid command name")
+
+        return "your result is: "+result;
+    }
+}
